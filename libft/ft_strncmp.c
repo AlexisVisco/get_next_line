3b25e1d5829a26c86d8e38cb656986801c63eef1
Alexis Viscogliosi <aviscogl@z3r1p2.le-101.fr>
2017-11-21 16:14:36 +0100

    libft

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviscogl <aviscogl@student.le101.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/20 20:17:45 by aviscogl          #+#    #+#             */
+/*   Updated: 2017/11/14 16:19:55 by aviscogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	if (n == 0)
+		return (0);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	i = -1;
+	while (++i < n && *s1++ == *s2++ && !(*s1 == '\0' && *s2 == '\0'))
+		;
+	return (*((unsigned char *)s1 - 1) - *((unsigned char *)s2 - 1));
+}
---
+++ libft/math_sqrt.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math_sqrt.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviscogl <aviscogl@student.le101.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 11:02:10 by aviscogl          #+#    #+#             */
+/*   Updated: 2017/11/16 11:20:48 by aviscogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		my_sqrt(int nb, int target)
+{
+	if (nb * nb > target)
+		return (0);
+	else if (nb * nb == target)
+		return (nb);
+	else
+		return (my_sqrt(nb + 1, target));
+}
+
+int				math_sqrt(int nb)
+{
+	return (my_sqrt(1, nb));
+}
